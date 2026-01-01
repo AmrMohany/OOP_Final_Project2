7e2dfe14e23ba5a35f663787d1b87e41ef557f12
AmrMohany <amr.h.m.mohany@gmail.com>
2026-01-01 12:22:23 +0300

    Add getter and setter methods to Task class

--- src/com/taskmanagement/models/Task.java
+++ src/com/taskmanagement/models/Task.java
@@ -24,4 +24,49 @@ public class Task implements Completable {
         this.priority = priority;
         this.completed = false;
     }
+    
+    
+    public int getId() { //Gets the unique task ID
+        return id;
+    }
+    
+    
+    public String getTitle() { //Gets the task title
+        return title;
+    }
+    
+    
+    public void setTitle(String title) { //Sets the task title
+        this.title = title;
+    }
+    
+    
+    public String getDescription() { //Gets the task description
+        return description;
+    }
+    
+    
+    public void setDescription(String description) { //Sets the task description
+        this.description = description;
+    }
+    
+   
+    public LocalDate getDeadline() { //Gets the task deadline
+        return deadline;
+    }
+    
+    
+    public void setDeadline(LocalDate deadline) { //Sets the task deadline
+        this.deadline = deadline;
+    }
+    
+   
+    public Priority getPriority() { //Gets the task priority
+        return priority;
+    }
+    
+   
+    public void setPriority(Priority priority) { //Sets the task priority
+        this.priority = priority;
+    }
 }
