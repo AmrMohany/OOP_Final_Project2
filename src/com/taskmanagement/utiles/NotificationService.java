c01bef5d750b1b781eaf563ca8862783ba799216
AmrMohany <amr.h.m.mohany@gmail.com>
2026-01-07 01:02:19 +0300

    Not Important

--- src/com/taskmanagement/utiles/NotificationService.java
+++ src/com/taskmanagement/utiles/NotificationService.java
@@ -1,4 +1,4 @@
-package com.taskmanagement.utiles;
+	package com.taskmanagement.utiles;
 
 import com.taskmanagement.models.Task;
 
@@ -7,17 +7,17 @@ public class NotificationService {
     
 	
     public static void notifyTaskCreated(Task task) { // method to announce new tasks
-        System.out.println("✓ NOTIFICATION: Task created - " + task.getTitle());
-        System.out.println("  ID: " + task.getId() + 
-                         " | Priority: " + task.getPriority() + 
-                         " | Deadline: " + task.getDeadline());
+        System.out.println("✓ NOTIFICATION:\n" + "Task created : " + task.getTitle());
+        System.out.println("ID: " + task.getId() + "\n" + 
+                           "Priority: " + task.getPriority() + "\n" +
+                           "Deadline: " + task.getDeadline());
         System.out.println();
     }
     
     
     public static void notifyTaskCompleted(Task task) { // method to announce completed tasks
-        System.out.println("✓ NOTIFICATION: Task completed - " + task.getTitle());
-        System.out.println("  ID: " + task.getId() + " | Marked as DONE");
+        System.out.println("✓ NOTIFICATION:\n" + "Task completed - " + task.getTitle());
+        System.out.println("ID: " + task.getId() + " | Marked as DONE");
         System.out.println();
     }
 }
